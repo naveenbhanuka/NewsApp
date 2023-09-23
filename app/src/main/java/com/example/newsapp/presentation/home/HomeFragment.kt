a19ad76fba1e9f7371cdac79c05ff1f9a4fe76e9
naveenbhanuka <naveenb.emedia@gmail.com>
2023-09-23 22:22:11 +0530

    Home ViewModel implementation and get data

--- app/src/main/java/com/example/newsapp/presentation/home/HomeFragment.kt
+++ app/src/main/java/com/example/newsapp/presentation/home/HomeFragment.kt
@@ -5,11 +5,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.newsapp.data.datasource.dto.NewsResponse
 import com.example.newsapp.databinding.FragmentHomeBinding
 import com.example.newsapp.presentation.adapters.FilterAdapter
 import com.example.newsapp.presentation.adapters.LatestNewsAdapter
 import com.example.newsapp.presentation.adapters.NewsAdapter
+import com.example.newsapp.utill.Msg
+import com.example.newsapp.utill.Resource
+import com.example.newsapp.utill.extenctions.alert
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class HomeFragment : Fragment(), FilterAdapter.OnItemClickListener {
 
@@ -22,6 +28,7 @@ class HomeFragment : Fragment(), FilterAdapter.OnItemClickListener {
     private lateinit var filterAdapter: FilterAdapter
     private lateinit var latestNewsAdapter: LatestNewsAdapter
     private lateinit var newsAdapter: NewsAdapter
+    private val vm by viewModel<HomeViewModel>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -35,6 +42,67 @@ class HomeFragment : Fragment(), FilterAdapter.OnItemClickListener {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setupRecyclerViews()
+
+        vm.latestNews.observe(viewLifecycleOwner, Observer { observerGetLatestNews(it) })
+        vm.getAllNews.observe(viewLifecycleOwner, Observer { observerGetAllNews(it) })
+    }
+
+    private fun observerGetAllNews(resource: Resource<NewsResponse>) {
+        when (resource) {
+            is Resource.Success -> {
+                //   hideProgressBar()
+                resource.data?.let { newsResponse ->
+                    newsAdapter.submitList(newsResponse.articles)
+                }
+            }
+
+            is Resource.Error -> {
+//                    hideProgressBar()
+                resource.message?.let { message ->
+                    alert(
+                        Msg.ALERT,
+                        message
+                    ) {
+                        positiveButton(Msg.BUTTON_OK) {
+                        }
+                    }.show()
+                }
+            }
+
+            is Resource.Loading -> {
+                // showProgressBar()
+            }
+        }
+    }
+
+    private fun observerGetLatestNews(resource: Resource<NewsResponse>) {
+        when (resource) {
+            is Resource.Success -> {
+                //   hideProgressBar()
+                resource.data?.let { newsResponse ->
+                    latestNewsAdapter.submitList(newsResponse.articles)
+                }.also {
+                      vm.getAllNews("null")
+                }
+            }
+
+            is Resource.Error -> {
+//                    hideProgressBar()
+                resource.message?.let { message ->
+                    alert(
+                        Msg.ALERT,
+                        message
+                    ) {
+                        positiveButton(Msg.BUTTON_OK) {
+                        }
+                    }.show()
+                }
+            }
+
+            is Resource.Loading -> {
+                // showProgressBar()
+            }
+        }
     }
 
     private fun setupRecyclerViews() {
@@ -81,6 +149,11 @@ class HomeFragment : Fragment(), FilterAdapter.OnItemClickListener {
 
     override fun onFilterClick(item: String) {
 
+    }
+
+    override fun onResume() {
+        super.onResume()
+        vm.getLatestNews()
     }
 
 }
---
+++ app/src/main/java/com/example/newsapp/presentation/home/HomeViewModel.kt
@@ -0,0 +1,52 @@
+package com.example.newsapp.presentation.home
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.newsapp.data.datasource.dto.NewsResponse
+import com.example.newsapp.domain.usecase.NewsUseCase
+import com.example.newsapp.utill.Resource
+import kotlinx.coroutines.launch
+import retrofit2.Response
+
+class HomeViewModel(
+    private val newsUseCase: NewsUseCase
+) : ViewModel(){
+
+    val latestNews: MutableLiveData<Resource<NewsResponse>> = MutableLiveData()
+    var latestNewsPage = 1
+
+    val getAllNews: MutableLiveData<Resource<NewsResponse>> = MutableLiveData()
+    var getAllNewsPage = 1
+
+    fun getLatestNews() = viewModelScope.launch {
+        latestNews.postValue(Resource.Loading())
+        val response = newsUseCase.getLatestNews("us", latestNewsPage)
+        latestNews.postValue(handleLatestNewsResponse(response))
+    }
+
+    private fun handleLatestNewsResponse(response: Response<NewsResponse>) : Resource<NewsResponse> {
+        if(response.isSuccessful) {
+            response.body()?.let { resultResponse ->
+                return Resource.Success(resultResponse)
+            }
+        }
+        return Resource.Error(response.message())
+    }
+
+    fun getAllNews(searchQuery: String?) = viewModelScope.launch {
+        getAllNews.postValue(Resource.Loading())
+        val response = newsUseCase.getAllNews(searchQuery, getAllNewsPage)
+        getAllNews.postValue(handleSearchNewsResponse(response))
+    }
+
+    private fun handleSearchNewsResponse(response: Response<NewsResponse>) : Resource<NewsResponse> {
+        if(response.isSuccessful) {
+            response.body()?.let { resultResponse ->
+                return Resource.Success(resultResponse)
+            }
+        }
+        return Resource.Error(response.message())
+    }
+
+}
