ae8f61ceef9503926b091f60a0701096023b4fbf
naveenbhanuka <naveenb.emedia@gmail.com>
2023-09-24 14:24:34 +0530

    View All Latest News API implementation

--- app/src/main/java/com/example/newsapp/presentation/view_latest_news/ViewLatestNews.kt
+++ app/src/main/java/com/example/newsapp/presentation/view_latest_news/ViewLatestNews.kt
@@ -2,16 +2,24 @@ package com.example.newsapp.presentation.view_latest_news
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.newsapp.R
+import com.example.newsapp.data.datasource.dto.NewsResponse
 import com.example.newsapp.databinding.ActivityViewLatestNewsBinding
 import com.example.newsapp.presentation.adapters.ViewLatestNewsAdapter
+import com.example.newsapp.presentation.home.HomeViewModel
+import com.example.newsapp.utill.Msg
+import com.example.newsapp.utill.Resource
+import com.example.newsapp.utill.extenctions.alert
 import com.example.newsapp.utill.extenctions.setActionBar
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class ViewLatestNews : AppCompatActivity() {
 
     private lateinit var binding: ActivityViewLatestNewsBinding
     private lateinit var viewLatestNewsAdapter: ViewLatestNewsAdapter
+    private val vm by viewModel<HomeViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +27,36 @@ class ViewLatestNews : AppCompatActivity() {
         setContentView(binding.root)
 
         initUI()
+        vm.latestNews.observe(this, Observer { observerGetLatestNews(it) })
+        vm.getLatestNews(null)
+    }
+
+    private fun observerGetLatestNews(resource: Resource<NewsResponse>) {
+        when (resource) {
+            is Resource.Success -> {
+                //   hideProgressBar()
+                resource.data?.let { newsResponse ->
+                    viewLatestNewsAdapter.submitList(newsResponse.articles)
+                }
+            }
+
+            is Resource.Error -> {
+//                    hideProgressBar()
+                resource.message?.let { message ->
+                    alert(
+                        Msg.ALERT,
+                        message
+                    ) {
+                        positiveButton(Msg.BUTTON_OK) {
+                        }
+                    }.show()
+                }
+            }
+
+            is Resource.Loading -> {
+                // showProgressBar()
+            }
+        }
     }
 
     private fun initUI() {
